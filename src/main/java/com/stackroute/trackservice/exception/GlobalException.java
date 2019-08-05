51037e011d508049eb0d2e81283965235cf9720a
purimetla <rakeshpurimetla@gmail.com>
2019-08-05 07:47:59 +0530

    lombok added

--- src/main/java/com/stackroute/trackservice/exception/GlobalException.java
+++ src/main/java/com/stackroute/trackservice/exception/GlobalException.java
@@ -1,7 +1,6 @@
 package com.stackroute.trackservice.exception;
 
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalException {
     @ExceptionHandler(TrackNotFoundException.class)
-    public ResponseEntity<?> trackNotFound(TrackNotFoundException e){
+    public ResponseEntity<?> trackNotFound(TrackNotFoundException e) {
         return new ResponseEntity<String>("Track Not Found", HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(TrackAlreadyExistException.class)
-    public ResponseEntity<?> idAlreadyExists(TrackAlreadyExistException ex){
-        return new ResponseEntity<String>("Track Already exists",HttpStatus.NOT_ACCEPTABLE);
+    public ResponseEntity<?> idAlreadyExists(TrackAlreadyExistException ex) {
+        return new ResponseEntity<String>("Track Already exists", HttpStatus.NOT_ACCEPTABLE);
     }
 }
